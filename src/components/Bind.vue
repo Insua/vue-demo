41d570ef6e1c6eec1229fc02264f39d627c7d544
insua <insuaxe@gmail.com>
2017-05-04 14:12:53 +0800

    commit

---
+++ src/components/Bind.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <a :href="href">href</a>
+        <p :id="id">id</p>
+        <img :src="src" alt="图片">
+        <button :disabled="disabled">按钮</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                id: 2,
+                href: 'http://www.baidu.com',
+                src: 'https://cn.vuejs.org/images/logo.png',
+                disabled: true
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,8 @@ import Router from 'vue-router'
 // import Hello from '@/components/Hello'
 // import Text from '@/components/Text'
 // import VOnce from '@/components/VOnce'
-import PureHtml from '@/components/PureHtml'
+// import PureHtml from '@/components/PureHtml'
+import Bind from '@/components/Bind'
 
 Vue.use(Router)
 
@@ -11,8 +12,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'PureHtml',
-      component: PureHtml
+      name: 'Bind',
+      component: Bind
     }
   ]
 })
