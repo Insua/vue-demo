abbeae54b2e08389b4f4d0429f6a50e56e050c02
insua <insuaxe@gmail.com>
2017-05-04 11:48:28 +0800

    commit

---
+++ src/components/VOnce.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <p v-once>hello world</p>
+        <button @click="world='insua'">改变world值</button>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        data() {
+            return {
+                world: 'world'
+            }
+        }
+    }
+
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 // import Hello from '@/components/Hello'
-import Text from '@/components/Text'
+// import Text from '@/components/Text'
+import VOnce from '@/components/VOnce'
 
 Vue.use(Router)
 
@@ -9,8 +10,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Text',
-      component: Text
+      name: 'VOnce',
+      component: VOnce
     }
   ]
 })
