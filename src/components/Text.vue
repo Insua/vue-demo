8b082ea01fa3d0ec8edfb593304c6d30be99de8b
insua <insuaxe@gmail.com>
2017-05-04 11:41:56 +0800

    commit

---
+++ src/components/Text.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <p>hello {{ world }}</p>
+        <p v-text="'hello' + world"></p>
+        <p>{{ `hello ${world}` }}</p>
+        <p v-text="`hello ${world}`"></p>
+        <button @click="world='insua'">改变world值</button>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        data () {
+            return {
+                world: "world"
+            }
+        }
+    }
+
+</script>
