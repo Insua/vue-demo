ead31fa1dc22d8d039aede4c718af05e24ca3167
insua <insuaxe@gmail.com>
2017-05-06 10:35:00 +0800

    commit

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,14 @@
       <div>
           <my-button @click.native="buttonClick"></my-button>
       </div>
+      <div>
+          <ul>
+              <li v-for="item in list">
+                  <h1>{{ item }}</h1>
+              </li>
+          </ul>
+          <div @touchmove.prevent class="layer"></div>
+      </div>
     <!--<router-view></router-view>-->
   </div>
 </template>
@@ -18,17 +26,25 @@ export default {
       buttonClick(){
           alert('点击原生');
       }
+    },
+    data() {
+      return {
+          list: [1,2,3,4,5,6,7,8,9,10]
+      }
     }
 }
 </script>
 
 <style>
-#app {
+/*#app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+}*/
+body,html {width:100%;height:100%}
+h1{margin-top:10rem;}
+.layer{width:100%;height:100%;position:fixed;top:0;left:0;z-index:99;background:rgba(0,0,0,.5)}
 </style>
