7e9e94589b2e591fb70dc470effa501100b1ac65
insua <insuaxe@gmail.com>
2017-06-08 17:59:06 +0800

    I should get a raise for this.

--- src/components/ComponentVFor.vue
+++ src/components/ComponentVFor.vue
@@ -7,6 +7,10 @@
         <button @click="shift">在头部去除一条数据</button>
         <button @click="reverse">把所有数据进行反转</button>
         <button @click="clear">清除所有数据</button>
+        <button @click="filter">数组选取xxx</button>
+        <button @click="contact">数组进行合并</button>
+        <button @click="map">数组映射</button>
+        <button @click="slice">返回截取后的数组</button>
     </div>
 </template>
 
@@ -26,7 +30,12 @@
         },
         data () {
             return {
-                artList: []
+                artList: [],
+                list2:  [
+                    { id: 4,name : 'iii',startTime: '4小时前',content: 'uuu',good: 4},
+                    { id: 5,name : 'jjj',startTime: '5小时前',content: 'vvv',good: 5},
+                    { id: 6,name : 'kkk',startTime: '6小时前',content: 'www',good: 6}
+                ]
             }
         },
         methods: {
@@ -56,6 +65,22 @@
             },
             clear () {
                 this.artList = [];
+            },
+            filter () {
+                this.artList = this.artList.filter((item) => {
+                    return item.name === 'xxx' ;
+                })
+            },
+            contact () {
+                this.artList = [...this.artList,...this.list2]
+            },
+            map () {
+                this.artList = this.artList.map(item => {
+                    return `${item} map`
+                })
+            },
+            slice() {
+                this.artList = this.artList.slice(2);
             }
         }
     }
